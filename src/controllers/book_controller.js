6abe62cd2d703eceda2bf606cb316cca500deb07
Sergio Pineda <sergio.pinedas94@gmail.com>
2021-07-15 20:20:37 -0500

    Se crea un servicio para obtener los libros por categoria.

--- src/controllers/book_controller.js
+++ src/controllers/book_controller.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createBook, getAllBooks} = require('../services/book_services');
+const {createBook, getAllBooks, getByCategory} = require('../services/book_services');
 const schema = require('../utils/data_validator')
 
 const route = express.Router();
@@ -39,6 +39,20 @@ route.get("/all", (req, res) => {
     })
   }).catch(err => {
     res.status(400).json({
+      error: err
+    })
+  });
+});
+
+route.get('/:category', (req, res) => {
+  let result = getByCategory(req.params.category);
+  result.then(resultValue => {
+    res.json({
+      result: resultValue
+    })
+  }).catch(err => {
+    res.status(400).json({
+      info: "La categoria solicitada no existe, por favor intente otra categoria",
       error: err
     })
   });
